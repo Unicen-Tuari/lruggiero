7d160859bbc5c561c75b97d737db72bf17445e15
Lucas Ruggiero <lucas9005@gmail.com>
2015-09-08 14:16:51 -0300

    Fixed #10 Creado el Router y su archivo de configuracion

---
+++ config/router_config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Clase que Contiene Todos los Posibles Caminos a ser Seleccionados por el Router
+	class RouterConfig{
+		public static $ACTION = 'action';
+		public static $ACTION_HOME = 'home';
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Inclusion del Archivo de Configuracion del Router
+	REQUIRE_ONCE('config/router_config.php');
+
+// Inclusion del Controlador Principal
+	REQUIRE_ONCE('controller/main_controller');
+
+// Resolucion del Enrutamiento
+	if(!array_key_exists(RouterConfig::$ACTION, $_REQUEST) OR $_REQUEST[RouterConfig::$ACTION] === RouterConfig::$ACTION_HOME){
+		// Cargar la Home
+	}
+
+?>
