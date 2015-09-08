44dc553cbfcf9d9a5cf5a953dabd7624a44af316
Lucas Ruggiero <lucas9005@gmail.com>
2015-09-08 15:02:00 -0300

    Fixed #14 Corregido error de tipeo en la inclusion del controlador principal en el router

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	REQUIRE_ONCE('config/router_config.php');
 
 // Inclusion del Controlador Principal
-	REQUIRE_ONCE('controller/main_controller');
+	REQUIRE_ONCE('controller/main_controller.php');
 
 // Resolucion del Enrutamiento
 	if(!array_key_exists(RouterConfig::$ACTION, $_REQUEST) OR $_REQUEST[RouterConfig::$ACTION] === RouterConfig::$ACTION_HOME){
